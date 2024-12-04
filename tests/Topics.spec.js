da3e2a18127226d4198c9af4424ec30f4a7283e5
Praveen <praveen.k@xnara.com>
2024-12-04 08:46:43 +0530

    Added topics.spec.js

---
+++ tests/Topics.spec.js
@@ -0,0 +1,16 @@
+// goto(URL in string format) - Which is use to launch the url on the given browser
+// locator(selector which is locators) - Is use to identify the element on the webpage
+// fill() - This is a method which is use to enter the text in the input field on web page
+// pressSequentially() - which is use to enter character by character in the input field
+// click() - which use to perform click operation on an element
+// close() - which is use to close the browser
+// title() - Which return the title of the page. - page.title()
+// expect(locator or value) - Which perform assertion for test step or test case
+// toBeChecked() - Validate of the element is checked or not
+// check() - which checks/click on radio button and checkbox
+// selectOption() - which use to select the value from the drop down that have been developed using select tag
+// uncheck() - which uncheck the checkbox and radio button
+// first() - Which identify the first matching element out of all the matching element
+// last() - Which identify the last matching element out of all the matching element
+// nth(index) - Identify the indexed matching element. Index start with 0.
+// dblClick() - Double click on an element
